bdc96cbc787171546c25a0f5284d9d23a2cb72ce
Alexander Radchenko <radchenkosasha@gmail.com>
2018-02-28 13:20:44 +0700

    TryCatchThrow

---
+++ Src/TryCatchThrow/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TryCatchThrow
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(RangeRecursive(1, 10).Sum());
+            Console.ReadLine();
+        }
+
+        static IEnumerable<int> RangeRecursive(int b, int e)
+        {
+            if (b > e)
+                yield break;
+
+            yield return b;
+
+            foreach (var v in RangeRecursive(b + 1, e))
+                yield return v;
+        }
+    }
+}
